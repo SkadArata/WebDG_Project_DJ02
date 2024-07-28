e31e5783297f056206b4d19f74562b8355d4416a
marsez <marsez@gmail.com>
2024-07-28 23:56:19 +0300

    Django-01, часть 2 === DJ02

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
+
 urlpatterns = [
-    path('', views.index),
-    path('data', views.data),
-    path('test/', views.test, name='test-page'),
-    path('new', views.new)
+    path('', views.index, name='index'),
+    path('new/', views.new, name='new'),
+    path('data/', views.data, name='data'),
+    path('test/', views.test, name='test'),
 ]
--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -1,12 +1,13 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h2>28 июля 2024 года, воскресенье: это мой первый проект на Django</h2>")
-
+#    return HttpResponse("<h2>28 июля 2024 года, воскресенье: это мой первый проект на Django</h2>")
+    return render(request, 'main/index.html')
 def new(request):
-    return HttpResponse("<h3>Это вторая страница моего первого проекта на Django</h3>")
-
+#    return HttpResponse("<h3>Это вторая страница моего первого проекта на Django</h3>")
+    return render(request, 'main/new.html')
 def data(request):
     return HttpResponse("<h3>Data-страница моего первого проекта на Django</h3>")
 
